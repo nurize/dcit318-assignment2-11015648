ce6d8a679d29fa8de5c9bca23806bf93f27dc686
Eunice Dickson <sunshinebae439@gmail.com>
2025-08-08 14:17:30 +0000

    Implement Interface in C#

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Interface
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Interface Demo ===");
+
+            IMovable myCar = new Car();
+            myCar.Move();  
+
+            IMovable myBicycle = new Bicycle();
+            myBicycle.Move();  
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+// End of file: dcit318-assignment2-11015648/Inheritance/Program.cs
