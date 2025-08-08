c1cf0bf3c6374958da0ce18c0b1ea50e687345b8
Eunice Dickson <sunshinebae439@gmail.com>
2025-08-08 14:21:25 +0000

    Implement Inheritance and Method Override

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        genericAnimal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
+// End of file: dcit318-assignment2-11015648/Inheritance/Program.cs
